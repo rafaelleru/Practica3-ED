53ca416f33f3a32de4774e689c654d1870549bca
rafa <rafaelleru95103@gmail.com>
2016-11-05 12:07:49 +0100

    eliminada lista enlazada como nested class

--- include/Pila_max_list.h
+++ include/Pila_max_list.h
@@ -8,33 +8,6 @@ template <class T>
  */
 
 class PilaMax{
-    /**
-     * @brief Un elemento de la lista enlazada.
-     * representa un valor y almacena la posicion del siguiente elemento en la lista.
-     */
-
-  struct Elemento{
-    T valor;
-    Elemento* sig;
-  };
-
-  class Lista_enlazada{
-    /**
-     * @brief TDA lista enlazada
-     */
-  private:
-    int n;
-    Elemento* first;
-    Elemento* last;
-  public:
-    Lista_enlazada();
-    Lista_enlazada(T e);
-    ~Lista_enlazada();
-    void add(T e);
-    T at(int index);
-    //void delete(int index);
-    int size();
-  };
  private:
   T maximo;
  
