4453e87b89998dbc6314939a895090d678a5459d
rafa <rafaelleru95103@gmail.com>
2016-11-08 00:14:35 +0100

    completada pila_max_list

--- src/Pila_max_list.cpp
+++ src/Pila_max_list.cpp
@@ -1,64 +1,88 @@
 #include "../include/Pila_max_list.h"
-#include "../include/Lista_enlazada.h"
+
 
 //----------------------------------------------------------------------
 //                        PILA MAX
-//----------------------------------------------------------------------
-
-template <class T>
-PilaMax<T>::PilaMax(){
-  list();
-}
+//---------------------------------------------------------------------
 
 template <class T>
 PilaMax<T>::PilaMax(T e){
-  list(e);
-  maximo = e;
+  this->list(e);
+  this->maximo = e;
+}
+
+
+template <class T>
+PilaMax<T>::PilaMax(PilaMax<T>& p){
+  this->list = p.list;
+  this->maximo = p.maximo;
 }
 
 template <class T>
-PilaMax<T>::~PilaMax(){}
+PilaMax<T>::~PilaMax(){
+  this->~list();
+}
 
 template <class T>
 void PilaMax<T>::push(T e){
-  this->list.push(e);
-  if(e > maximo)
-    maximo = e;
+  this->pila.push(e);
+  if(e>this->maximo){
+    this->maximo = e;
+  }
 }
 
 template <class T>
 T PilaMax<T>::pop(){
-  maximo = list.at(0);
-  T to_ret = list.getLast();
-  T max;
-  if(to_ret = maximo){
-    for(int i=0; i < list.size(); ++i){
-      max = list.at(i);
-      if(max > maximo){
-	maximo = max;
+  if (this->list.getLast() == maximo){
+    this->maximo = 0;
+    for(int i = 0; i < this->list.size(); i++){
+      if(this->list.at(i) > maximo){
+	maximo = this->list.at(i);
       }
     }
   }
 
+  T to_ret = this->list.getLast();
+  this->list.remove(this->size());
   return to_ret;
 }
 
+
 template <class T>
-PilaMax<T>& PilaMax<T>::operator=(PilaMax<T> p){
-  if(this != p){
+int PilaMax<T>::size(){
+  return this->list.size();
+}
+
+template <class T>
+T PilaMax<T>::getMax(){
+  return this->maximo;
+}
+
+template <class T>
+PilaMax<T>& PilaMax<T>::operator=(PilaMax<T>& p){
+  if( this!= p){
     this->list = p.list;
     this->maximo = p.maximo;
   }
-  
+
   return this;
 }
 
 template <class T>
-bool PilaMax<T>::operator==(PilaMax<T> &p){
-  return this->list == p.list && this->maximo == p.maximo;
+bool PilaMax<T>::operator==(PilaMax<T>& p){
+  if(this->maximo == p.maximo){
+    bool equal = true;
+    for(int i = 0; i < this->list.size() && true; ++i){
+      if(this->list.at(i) != p.list.at(i)){
+	equal = false;
+      }
+    }
+
+    return equal;
+  }else return false;
 }
 
 template <class T>
-bool PilaMax<T>::operator!=(PilaMax<T> &p){
-  return this != p;
+bool PilaMax<T>::operator!=(PilaMax<T>& p){
+  return !(this == p);
 }
