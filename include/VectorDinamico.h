66980b41a2e368d696220df7abe0bebcadfd9fc2
adriordi <adrihuelma22@gmail.com>
2016-11-08 11:58:05 +0100

    Primera version .h y .cpp de Vector_Dinamico

---
+++ include/VectorDinamico.h
@@ -0,0 +1,27 @@
+#ifndef __VECTOR_DINAMICO__
+#define __VECTOR_DINAMICO__
+
+using namespace std;
+
+template <class T>
+
+class Vector_Dinamico{
+ private:
+  T *datos;
+  int reservados;
+  int utilizados;
+ public:
+  Vector_Dinamico();
+  Vector_Dinamico(const Vector_Dinamica& vd);
+  ~Vector_Dinamico();
+  Vector_Dinamico& operator= (const Vector_Dinamico& vd);
+
+  void resize(int n);
+  bool vacia() const{ return nelem == 0;}
+  void poner(T c);
+  void quitar();
+  T tope() const;
+  
+};
+
+#endif
